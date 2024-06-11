c4cba208ef67122d7a421a8e710c625201fc27d6
shreyasgowda2003 <161202582+shreyasgowda2003@users.noreply.github.com>
2024-06-11 22:13:09 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,33 @@
+# a="king for a reason"
+# print(a[-10:16])
+
+# a="king"
+# b="KING"
+# print(a.upper())
+# print(b.lower())
+
+# a="king v k"
+# print(a.replace('i','a'))
+
+# a="king v k"
+# print(a.split('i',))
+ 
+# text='"My name \"is\" shreays"'
+# print(text)
+
+# a=["A","B","C"]
+# a.insert(1,"D")
+# print(a)
+
+# a=[55,4,75,2]
+# a.sort(reverse=True)
+# print(a)
+
+# a=[1,2,3,4]
+# b=a.copy()
+# print(b)
+
+a=[1,2]
+b=[3,4]
+a.extend(b)
+print(a)
